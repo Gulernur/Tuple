cd799439b2f02a5e568108c4622a07fda11e2b87
gulernurdonmez <gule0153@stud.kea.dk>
2023-03-03 19:59:25 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations; 
+namespace Tuple
+{
+    class Person
+    {
+        public string? Name {get; set;}
+        public int Age {get; set;}
+        public string? Occupation {get; set;}
+        public decimal Salary {get; set;}
+
+
+        public Tuple<string, int, string, decimal> GetPersonInfo(string name, int age, string occupation, decimal salary)
+        {
+            return new Tuple<string, int, string, decimal>(name, age, occupation, salary);
+        }
+    }
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
+}
