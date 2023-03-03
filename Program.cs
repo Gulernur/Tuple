98f242540415a7d21d4371e8411b9220f8f30080
gulernurdonmez <gule0153@stud.kea.dk>
2023-03-03 20:16:32 +0100

    added test data

--- Program.cs
+++ Program.cs
@@ -9,19 +9,31 @@ namespace Tuple
         public string? Occupation { get; set; }
         public decimal Salary { get; set; }
 
-        public Tuple<string, int, string, decimal> GetPersonInfo(
-            string name,
-            int age,
-            string occupation,
-            decimal salary
-        )
+        public Person (string name, int age, string occupation, decimal salary)
         {
-            return new Tuple<string, int, string, decimal>(name, age, occupation, salary);
+            this.Name = name;
+            this.Age = age;
+            this.Occupation = occupation; 
+            this.Salary = salary;
+        }
+
+        public Tuple<string, int, string, decimal> GetPersonInfo(Person person)
+        {
+            return new Tuple<string, int, string, decimal>(person.Name, person.Age, person.Occupation, person.Salary);
         }
     }
 
     class Program
     {
-        static void Main(string[] args) { }
+        static void Main(string[] args)
+        {
+            Person person = new Person("John", 30, "Software Engineer", 75000.00m);
+
+            var (name, age, occupation, salary) = person.GetPersonInfo(person);
+
+            Console.WriteLine(
+                $"{name} is {age} years old and works as a {occupation}, earning {salary:C} per year."
+            );
+        }
     }
 }
